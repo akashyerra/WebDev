c1de0a292716e0d76dc00c7cebccec746e444684
akashyerra <akashyerra@gmail.com>
2024-02-06 10:56:27 +0530

    Day 1 practice

---
+++ JS/test.js
@@ -0,0 +1,6 @@
+console.log("Yerra Akash");
+
+// .js extention is used to represent the javascript files
+// Every programming language has its own extentions
+// In the early days js compiler used to be embedded with the browsers, hence we had to create a index.html page to launch the js code. But now in the present day the compilers have been extracted out. The most famous js compilers are nodejs and denojs compilers. Nodejs is the most popular one and has higher support
+// In nodejs the current version is used for the learning purpose and the lts(long term support) version is used for the development of the big softwares etc.
