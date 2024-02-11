8df543ab111b9703eaa51a56d04b88782e8a8cac
akashyerra <akashyerra@gmail.com>
2024-02-11 20:44:09 +0530

    strings learned

--- JS/05_strings.js
+++ JS/05_strings.js
@@ -9,9 +9,30 @@ console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); //thi
 const gameName = new String("Akash yerra");
 
 console.log(gameName[0]);
-console.log(gameName.__proto__)
+console.log(gameName.__proto__) // in the current program it shows that the object is empty but when we run in the browser's console it is a object containing alphabets
 
 console.log(gameName.length);
-console.log(gameName.toUpperCase());
-console.log(gameName.charAt(6));
+console.log(gameName.toUpperCase()); // this doesn't change the original value as the string is an primitive datatype 
+console.log(gameName.charAt(6)); 
+console.log(gameName.endsWith('a'));
+console.log(gameName.indexOf('a')); // returns the index at which the given alphabet is first occured
 
+// knowing the maximum number of methods of the string is very helpful as it is quiet useful in case of dsa
+
+const newString = gameName.substring(0,4);
+console.log(newString);
+
+const anotherString = gameName.slice(1, 4);
+console.log(anotherString);
+
+const newStr1 = "     Akash      ";
+console.log(newStr1);
+console.log(newStr1.trim());
+
+const url = "https://akash.com/akash%20yerra";
+
+console.log(url.replace('%20', '-'));
+
+console.log(url.includes('akash'));
+
+console.log(gameName.split(' '));
