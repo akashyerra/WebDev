083de71a71c630a6deacd7f4c4609178f5d4dee6
akashyerra <akashyerra@gmail.com>
2024-02-16 22:27:01 +0530

    functions in javascript learned

--- JS/03_Basics/01_functions.js
+++ JS/03_Basics/01_functions.js
@@ -41,3 +41,37 @@ function loginMessage(username = "RDJ" /* it is default value -> when the functi
 }
 console.log(loginMessage("akash"));
 console.log(loginMessage());
+console.log();
+
+function calculateCartPrice(val1, val2, ...num1) /* rest operator used */ {
+    return num1;
+}
+console.log(calculateCartPrice(2));
+// suppose if more number of inputs are there for the function, we use rest operator denoted by "..."
+// rest allows a function to accept an indefinite number of arguments as an array, providing a way to represent variadic functions in JavaScript.
+
+console.log(calculateCartPrice(200, 400, 500, 2000));
+
+const user = {
+    username: "akash",
+    prices: 199
+}
+
+function handleObject (anyObject) {
+    console.log(`username is ${anyObject.username} and price is ${anyObject.price}`);
+}
+
+handleObject(user);
+
+handleObject({
+    username: "akshay",
+    price: 399
+})
+
+const myNewArray = [200, 400, 500, 600]
+
+function returnSecondValue(getArray) {
+    return getArray[1];
+}
+console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([500, 600, 700, 800]));
