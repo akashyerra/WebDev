1abaeec5194fe2c89908e76dd8a40168ac36372f
akashyerra <akashyerra@gmail.com>
2024-02-18 00:28:32 +0530

    scope levels and mini hoisting learned

--- JS/03_Basics/02_scopes.js
+++ JS/03_Basics/02_scopes.js
@@ -13,6 +13,45 @@ if (true) {
 
 // {block scope - scope of the code written in this braces is defined by these braces, if any variable or function defined in this braces, is being accessed outside the braces then it doesnt work}
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+/************************Nested Scope***************************************/
+
+function one(){
+  const username = "Akash"
+  
+  function two() {
+    const website = "youtube"
+    console.log(username);
+  }
+
+  // console.log(website);
+  two();
+}
+
+one();
+
+if(true){
+  const username = "Akash";
+  if(username == "Akash"){
+    const website = "youtube";
+    console.log(username +" "+ website);
+  }
+  // console.log(website);
+}
+// console.log(username);
+
+/************************************************ INTRESTING ****************************************/
+// Hoisting is the study of the functions what are the types and how will they be executed
+
+console.log(addOne(9));
+function addOne(num){
+  return num+1;
+}
+
+console.log(addTwo(7));
+const addTwo = function(num){
+  return num + 2;
+}
