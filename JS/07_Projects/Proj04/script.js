512a6c024d79ec8e9e43412da4877d7ef689a924
akashyerra <akashyerra@gmail.com>
2024-04-29 21:42:35 +0530

    Project 4 updated

---
+++ JS/07_Projects/Proj04/script.js
@@ -0,0 +1,32 @@
+const randomNumber = parseInt(Math.random() * 100 + 1);
+
+const submit = document.querySelector('#submt');
+const userInput = document.querySelector('#guessNumber');
+const guessSlot = document.querySelector('.guesses');
+const remaining = document.querySelector('.remaining');
+const lowOrHigh = document.querySelector('.lowOrHigh');
+const startOver = document.querySelector('.resultSpace');
+
+const p = document.createElement('p');
+
+let prevGuess = [];
+let numGuess = 1;
+
+let playGame = true;
+
+function validateGuess(guess) {
+    //
+}
+
+function checkGuess(guess) {
+    //
+}
+
+function displayGuess(guess) {
+    //
+}
+
+function displayMessage(message){
+    //
+}
+
