fbf1c1b10158685c6c3e961a4372d66e78817f39
akashyerra <akashyerra@gmail.com>
2024-04-30 20:58:01 +0530

    Project 4 updated

--- JS/07_Projects/Proj04/script.js
+++ JS/07_Projects/Proj04/script.js
@@ -14,19 +14,65 @@ let numGuess = 1;
 
 let playGame = true;
 
+if(playGame){
+    submit.addEventListener('click', function(e){
+        e.preventDefault();
+        const guess = parseInt(userInput.value);
+        console.log(guess);
+        validateGuess(guess);
+    });
+}
+
 function validateGuess(guess) {
-    //
+    if(isNaN(guess)){
+        alert('Please enter a valid number');
+    }
+    else if(guess < 1){
+        alert("Please enter a number greater than 1");
+    }
+    else if(guess > 100){
+        alert('Please enter a number less than 100');
+    }
+    else{
+        prevGuess.push(guess);
+        if(numGuess === 11) {
+            displayGuess(guess);
+            displayMessage(`Game Over. Random number was ${randomNumber}`);
+            endGame();
+        }
+        else {
+            displayGuess(guess);
+            checkGuess(guess);
+        }
+    }
 }
 
 function checkGuess(guess) {
-    //
+    if(guess === randomNumber){
+        displayMessage(`You guessed it right`);
+        endGame();
+    }
+    else if(guess < randomNumber) {
+        displayMessage(`Number is TOO low`);
+    }
+    else if(guess > randomNumber) {
+        displayMessage(`Number is TOO high`);
+    }
 }
 
 function displayGuess(guess) {
-    //
+    
 }
 
 function displayMessage(message){
+    //
+}
+
+function endGame() {
+    //
+}
+
+function newGame() {
     //
 }
 
